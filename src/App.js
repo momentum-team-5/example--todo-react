9f708e23ce70344f72c5311500ed638f1eacf68c
Clinton Dreisbach <clinton@dreisbach.us>
2020-11-11 22:05:47 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'tachyons'
+import './App.css'
 
-function App() {
+const todos = [
+  { id: 1, done: true, task: 'Take out the papers' },
+  { id: 2, done: true, task: 'Take out the trash' },
+  { id: 3, done: false, task: 'Get some spending cash' }
+]
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App mw8 pa2 center'>
+      <h1>My todos</h1>
+
+      <div>
+        <span id='left'>
+          {todos.filter(todo => !todo.done).length}
+        </span> left{' '}
+        <span id='done'>
+          {todos.filter(todo => todo.done).length}
+        </span> done
+      </div>
+
+      <ul className='list pl0' id='todos'>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <label>
+              <input type='checkbox' checked={todo.done} /> {todo.task}
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
